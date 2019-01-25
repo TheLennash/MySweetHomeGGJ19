52d19b124f8e177d71ce9632fa96cc59a660c3c8
Lennart Neelis <lennart.neelis@breinwave.nl>
2019-01-25 22:59:23 +0100

    CameraRig

---
+++ MySweetHome/Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public bool DoFollow = true;
+
+    public Transform Gyro;
+
+    public GameObject Target;
+
+
+    public float smoothing = 4f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!DoFollow)
+            return;
+
+        if (Target == null)
+            return;
+
+        Gyro.LookAt(Target.transform);
+
+        Quaternion Wanted = Quaternion.Euler(new Vector3(0,Gyro.eulerAngles.y, 0));
+
+        this.transform.rotation = Quaternion.Lerp(this.transform.rotation, Wanted, Time.deltaTime * smoothing);
+    }
+
+
+
+}
