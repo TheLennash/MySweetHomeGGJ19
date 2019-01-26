92dad37566763890d1d229cd1fabf0395a088a4b
Lennart Neelis <lennart.neelis@breinwave.nl>
2019-01-26 10:26:39 +0100

    kids update

--- MySweetHome/Assets/Scripts/Candy/CandyCane.cs
+++ MySweetHome/Assets/Scripts/Candy/CandyCane.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CandyCane : MonoBehaviour
+public class CandyCane : Candy
 {
     // Start is called before the first frame update
     void Start()
--- MySweetHome/Assets/Scripts/HouseBehaviour.cs
+++ MySweetHome/Assets/Scripts/HouseBehaviour.cs
@@ -1,13 +1,40 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class HouseBehaviour : MonoBehaviour
 {
     public List<WallBehaviour> Walls = new List<WallBehaviour>();
 
+    public WallBehaviour GetTargetWall(KidScript kid)
+    {
+        WallBehaviour result = null;
+        float closest = float.MaxValue;
+
+        foreach (var wall in Walls)
+        {
+            if (wall.GetCandies(kid.CandyPrefrence) > 0)
+            {
+                var distance = Vector3.Distance(wall.transform.position, kid.transform.position);
+                if (distance < closest)
+                {
+                    result = wall;
+                    closest = distance;
+
+
+                }
+            }
+        }
+
+
+        if (result != null)
+            return result;
+
+        Debug.Log("Failed to find wall");
+        return Walls[0];
+    }
+
     private void Awake()
     {
-        
+
     }
 }
