ea749ee5b2de8885fde80cfea07f3757e8a03302
Ruben Hooijer <loljoch@gmail.com>
2019-01-26 09:06:52 +0100

    PlayerPREFAB

--- MySweetHome/Assets/Scripts/playerScript.cs
+++ MySweetHome/Assets/Scripts/playerScript.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerScript : MonoBehaviour{
+public class playerScript : MonoBehaviour {
 
     public Transform Rotation;
+    public Transform child;
+    public Transform house;
 
     public float speed;
-    private bool canGrab = false;
+
+
 
     // Start is called before the first frame update
     void Start() {
-        
+
     }
 
     // Update is called once per frame
@@ -24,25 +27,58 @@ public class playerScript : MonoBehaviour{
 
     void PlayerMovement() {
 
+        //distance between player and house
+        
+
+        //Defines player movement direction
         var horizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
         var vertical = Input.GetAxis("Vertical") * speed * Time.deltaTime;
 
-        transform.Translate(-horizontal, 0, -vertical);
 
-    }
+        var moveDir = new Vector3(-horizontal, 0, -vertical);
 
-    void GrabKid() {
-
+        var angle = Vector3.Angle(moveDir, transform.forward); //
 
 
-    }
+        
+        transform.Translate(moveDir);
+        
 
-    //Defines if player can grab kid
-    private void OnCollisionEnter(Collision col) {
-        if (col.gameObject.tag == "Kid") {
-            canGrab = true;
+
+        //child.transform.eulerAngles =  new Vector3(0,angle, 0);
+
+
+        //Rotate child
+        //Rotate left
+        if (horizontal < 0) {
+            child.transform.localEulerAngles = new Vector3(-horizontal, transform.rotation.y + 90, -vertical);
+        }
+
+        //Rotate right
+        if (horizontal > 0) {
+            child.transform.localEulerAngles = new Vector3(-horizontal, transform.rotation.y - 90, -vertical);
+        }
+
+        //Rotate forward
+        if (vertical < 0) {
+            child.transform.localEulerAngles = new Vector3(-horizontal, transform.rotation.y, -vertical);
+        }
+
+        if (vertical > 0) {
+            child.transform.localEulerAngles = new Vector3(-horizontal, transform.rotation.y + 180, -vertical);
         }
 
 
     }
+
+
+    //Defines if player can grab kid
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log("hit building");
+
+        if (other.gameObject.tag == "Building") {
+          
+        }
+    }
+
 }
